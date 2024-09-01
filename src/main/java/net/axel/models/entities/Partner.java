062e46f8e207b9571997857f1505be8921a34382
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2024-09-01 21:50:23 +0100

    set constructor for Partner

--- src/main/java/net/axel/models/entities/Partner.java
+++ src/main/java/net/axel/models/entities/Partner.java
@@ -1,5 +1,8 @@
 package net.axel.models.entities;
 
+import net.axel.models.enums.PartnerStatus;
+import net.axel.models.enums.TransportType;
+
 import java.util.Date;
 import java.util.UUID;
 
@@ -7,10 +10,20 @@ public class Partner {
     private UUID id;
     private String companyName;
     private String commercialContact;
-    private String transportType;
+    private TransportType transportType;
     private String geographicalArea;
     private String specialConditions;
-    private String partnerStatus;
+    private PartnerStatus partnerStatus;
     private Date creationDate;
+
+    public Partner(UUID id, String companyName, String commercialContact, TransportType transportType, String geographicalArea, String specialConditions, PartnerStatus partnerStatus) {
+        this.id = id;
+        this.companyName = companyName;
+        this.commercialContact = commercialContact;
+        this.transportType = transportType;
+        this.geographicalArea = geographicalArea;
+        this.specialConditions = specialConditions;
+        this.partnerStatus = partnerStatus;
+    }
 
 }
