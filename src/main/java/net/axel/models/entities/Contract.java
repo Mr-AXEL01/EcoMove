53ca68b745ae9ad8170a8fd1e0e6452f87bf6d87
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2024-09-01 22:09:50 +0100

    set attribut and constracutor for contract

--- src/main/java/net/axel/models/entities/Contract.java
+++ src/main/java/net/axel/models/entities/Contract.java
@@ -1,4 +1,26 @@
 package net.axel.models.entities;
 
+import net.axel.models.enums.ContractStatus;
+
+import java.util.Date;
+import java.util.UUID;
+
 public class Contract {
+    private UUID id;
+    private Date startDate;
+    private Date endDate;
+    private float specialTariff;
+    private String conditionsAccord;
+    private Boolean renewable;
+    private ContractStatus contractStatus;
+
+    public Contract(UUID id, Date startDate, Date endDate, float specialTariff, String conditionsAccord, Boolean renewable, ContractStatus contractStatus) {
+        this.id = id;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.specialTariff = specialTariff;
+        this.conditionsAccord = conditionsAccord;
+        this.renewable = renewable;
+        this.contractStatus = contractStatus;
+    }
 }
