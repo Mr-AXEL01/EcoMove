ba5208b1ac1a777216bc85d8826c63651b4186f5
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2024-09-01 22:18:30 +0100

    set attribut and constructor for Promotion

--- src/main/java/net/axel/models/entities/Promotion.java
+++ src/main/java/net/axel/models/entities/Promotion.java
@@ -1,4 +1,30 @@
 package net.axel.models.entities;
 
+import org.w3c.dom.Text;
+
+import java.util.Date;
+import java.util.UUID;
+
 public class Promotion {
+    private UUID id;
+    private String offerName;
+    private String description;
+    private Date startDate;
+    private Date endDate;
+    private ReductionType reductionType;
+    private float reductionValue;
+    private String conditions;
+    private OfferStatus offerStatus;
+
+    public Promotion(UUID id, String offerName, String description, Date startDate, Date endDate, ReductionType reductionType, float reductionValue, String conditions, OfferStatus offerStatus) {
+        this.id = id;
+        this.offerName = offerName;
+        this.description = description;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.reductionType = reductionType;
+        this.reductionValue = reductionValue;
+        this.conditions = conditions;
+        this.offerStatus = offerStatus;
+    }
 }
