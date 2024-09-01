67bf4df856dcb76355615e628212b0374741978c
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2024-09-01 22:56:00 +0100

    set setter and getter for ticket

--- src/main/java/net/axel/models/entities/Ticket.java
+++ src/main/java/net/axel/models/entities/Ticket.java
@@ -1,5 +1,6 @@
 package net.axel.models.entities;
 
+import net.axel.models.enums.TicketStatus;
 import net.axel.models.enums.TransportType;
 
 import java.util.Date;
@@ -19,6 +20,54 @@ public class Ticket {
         this.purchasePrice = purchasePrice;
         this.resellPrice = resellPrice;
         this.saleDate = saleDate;
+        this.ticketStatus = ticketStatus;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public TransportType getTransportType() {
+        return transportType;
+    }
+
+    public void setTransportType(TransportType transportType) {
+        this.transportType = transportType;
+    }
+
+    public float getPurchasePrice() {
+        return purchasePrice;
+    }
+
+    public void setPurchasePrice(float purchasePrice) {
+        this.purchasePrice = purchasePrice;
+    }
+
+    public float getResellPrice() {
+        return resellPrice;
+    }
+
+    public void setResellPrice(float resellPrice) {
+        this.resellPrice = resellPrice;
+    }
+
+    public Date getSaleDate() {
+        return saleDate;
+    }
+
+    public void setSaleDate(Date saleDate) {
+        this.saleDate = saleDate;
+    }
+
+    public TicketStatus getTicketStatus() {
+        return ticketStatus;
+    }
+
+    public void setTicketStatus(TicketStatus ticketStatus) {
         this.ticketStatus = ticketStatus;
     }
 }
