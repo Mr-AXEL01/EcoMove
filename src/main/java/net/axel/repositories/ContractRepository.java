2d015391cc3df84f734a2618b68e5071791b4bfa
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2024-09-02 20:46:42 +0100

    set the addContract for the Contract

--- src/main/java/net/axel/repositories/ContractRepository.java
+++ src/main/java/net/axel/repositories/ContractRepository.java
@@ -1,4 +1,32 @@
 package net.axel.repositories;
 
+import net.axel.config.DatabaseConnection;
+import net.axel.models.entities.Contract;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Date;
+
 public class ContractRepository {
+
+    private final String tableName = "contract";
+    private final Connection connection = DatabaseConnection.getInstance().getConnection();
+
+    public ContractRepository() throws SQLException {
+    }
+    public void addContract(Contract contract) throws  SQLException{
+        String query = "INSERT INTO " + tableName + " (id, start_date, end_date, special_tariff, conditions_accord, renewable, contract_status, partner) " +
+                "VALUES (?, ?, ?, ?, ?, ?, ?::ContractStatus, ?)";
+        try(PreparedStatement stmt = connection.prepareStatement(query)) {
+            stmt.setObject(1, contract.getId());
+            stmt.setDate(2, new Date(contract.getStartDate().getTime()));
+            stmt.setDate(3, new Date(contract.getEndDate().getTime()));
+            stmt.setDouble(4, contract.getSpecialTariff());
+            stmt.setString(5, contract.getConditionsAccord());
+            stmt.setBoolean(6, contract.getRenewable());
+            stmt.setString(7, contract.getContractStatus().name());
+            stmt.setObject(8, contract.getPartner());
+        }
+    }
 }
