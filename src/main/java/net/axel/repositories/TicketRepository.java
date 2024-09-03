bb8a36dd49907d47e5156503995613624c9d2611
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2024-09-04 00:07:04 +0100

    handle the add method for the ticket

--- src/main/java/net/axel/repositories/TicketRepository.java
+++ src/main/java/net/axel/repositories/TicketRepository.java
@@ -1,4 +1,39 @@
 package net.axel.repositories;
 
+import net.axel.config.DatabaseConnection;
+import net.axel.models.entities.Ticket;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
 public class TicketRepository {
+
+    private final String tableName = "ticket";
+    private final Connection connection = DatabaseConnection.getInstance().getConnection();
+
+    public TicketRepository() throws SQLException{
+    }
+
+    public void addTicket(Ticket ticket) throws SQLException{
+        String query = "INSERT INTO " + tableName + " id, transport_type, purchase_price, resell_price, sale_date, ticket_status, contract_id " +
+                "VALUES(?, ?::TransportType, ?, ?, ?, ?::TicketStatus, ?)";
+        try (PreparedStatement stmt = connection.prepareStatement(query)){
+            stmt.setObject(1, ticket.getId());
+            stmt.setString(2, ticket.getTransportType().name());
+            stmt.setDouble(3, ticket.getPurchasePrice());
+            stmt.setDouble(4, ticket.getResellPrice());
+            stmt.setDate(5, new Date(ticket.getSaleDate().getTime()));
+            stmt.setString(6, ticket.getTicketStatus().name());
+            stmt.setObject(7, ticket.getContract().getId());
+            stmt.executeUpdate();
+        }
+    }
+    public List<Ticket> getAllTickets() throws SQLException {
+        List<Ticket> tickets = new ArrayList<>();
+//        to do
+    }
+    public List<Ticket> getTicketsByPartner() throws SQLException {
+//        to do
+    }
 }
