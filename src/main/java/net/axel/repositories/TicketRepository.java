bd7207af72cc98747d9b82a317bd8c587f6668e5
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2024-09-04 15:56:00 +0100

    set the method for the add amd get all tickets

--- src/main/java/net/axel/repositories/TicketRepository.java
+++ src/main/java/net/axel/repositories/TicketRepository.java
@@ -1,11 +1,18 @@
 package net.axel.repositories;
 
 import net.axel.config.DatabaseConnection;
+import net.axel.models.entities.Contract;
+import net.axel.models.entities.Partner;
 import net.axel.models.entities.Ticket;
+import net.axel.models.enums.ContractStatus;
+import net.axel.models.enums.PartnerStatus;
+import net.axel.models.enums.TicketStatus;
+import net.axel.models.enums.TransportType;
 
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 public class TicketRepository {
 
@@ -16,7 +23,7 @@ public class TicketRepository {
     }
 
     public void addTicket(Ticket ticket) throws SQLException{
-        String query = "INSERT INTO " + tableName + " id, transport_type, purchase_price, resell_price, sale_date, ticket_status, contract_id " +
+        String query = "INSERT INTO " + tableName + " (id, transport_type, purchase_price, resell_price, sale_date, ticket_status, contract_id) " +
                 "VALUES(?, ?::TransportType, ?, ?, ?, ?::TicketStatus, ?)";
         try (PreparedStatement stmt = connection.prepareStatement(query)){
             stmt.setObject(1, ticket.getId());
@@ -29,11 +36,65 @@ public class TicketRepository {
             stmt.executeUpdate();
         }
     }
+
     public List<Ticket> getAllTickets() throws SQLException {
         List<Ticket> tickets = new ArrayList<>();
-//        to do
+        String query = "SELECT t.*, c.*, p.* FROM " + tableName + " t " +
+                "JOIN contract c ON t.contract_id = c.id " +
+                "JOIN partner p ON c.partner_id = p.id";
+        try (PreparedStatement stmt = connection.prepareStatement(query)) {
+            ResultSet resultSet = stmt.executeQuery();
+            while(resultSet.next()) {
+                tickets.add(mapToTicket(resultSet));
+            }
+        }
+        return tickets;
     }
-    public List<Ticket> getTicketsByPartner() throws SQLException {
-//        to do
+
+    public List<Ticket> getTicketsByPartner(UUID id) throws SQLException {
+        List<Ticket> partnerTickets = new ArrayList<>();
+        String query = "SELECT t.*, c.*, p.* FROM " + tableName + " t " +
+                "JOIN contract c ON t.contract_id = c.id " +
+                "JOIN partner p ON c.partner_id = p.id WHERE p.id = ?";
+        try (PreparedStatement stmt = connection.prepareStatement(query)) {
+            stmt.setObject(1, id);
+            ResultSet resultSet = stmt.executeQuery();
+            while(resultSet.next()) {
+                partnerTickets.add(mapToTicket(resultSet));
+            }
+        }
+        return partnerTickets;
+    }
+
+    private Ticket mapToTicket(ResultSet resultSet) throws SQLException {
+        UUID ticketId = UUID.fromString(resultSet.getString("id"));
+        TransportType transportType  = TransportType.valueOf(resultSet.getString("transport_type"));
+        Double purchasePrice = resultSet.getDouble("purchase_price");
+        Double resellPrice = resultSet.getDouble("resell_price");
+        Date saleDate = resultSet.getDate("sale_date");
+        TicketStatus ticketStatus = TicketStatus.valueOf(resultSet.getString("ticket_status"));
+
+        UUID contractId = UUID.fromString(resultSet.getString("contract_id"));
+        Date startDate = resultSet.getDate("start_date");
+        Date endDate = resultSet.getDate("end_date");
+        double specialTariff = resultSet.getDouble("special_tariff");
+        String conditionsAccord = resultSet.getString("conditions_accord");
+        Boolean renewable = resultSet.getBoolean("renewable");
+        ContractStatus contractStatus = ContractStatus.valueOf(resultSet.getString("contract_status"));
+
+        UUID partnerId = UUID.fromString(resultSet.getString("partner_id"));
+        String companyName = resultSet.getString("company_name");
+        String comercialContact = resultSet.getString("comercial_contact");
+        String geographicalArea = resultSet.getString("geographical_area");
+        String specialConditions = resultSet.getString("special_conditions");
+        TransportType partnerTransportType = TransportType.valueOf(resultSet.getString("transport_type"));
+        PartnerStatus partnerStatus = PartnerStatus.valueOf(resultSet.getString("partner_status"));
+        Date creationDate = resultSet.getDate("creation_date");
+
+        Partner partner = new Partner(partnerId, companyName, comercialContact, partnerTransportType, geographicalArea, specialConditions, partnerStatus, creationDate);
+
+        Contract contract = new Contract(contractId, startDate, endDate, specialTariff, conditionsAccord, renewable, contractStatus, partner);
+
+        return new Ticket(ticketId, transportType, purchasePrice, resellPrice, saleDate, ticketStatus, contract);
     }
 }
--- src/main/java/net/axel/services/TicketService.java
+++ src/main/java/net/axel/services/TicketService.java
@@ -1,4 +1,76 @@
 package net.axel.services;
 
+import net.axel.models.dto.TicketDto;
+import net.axel.models.entities.Contract;
+import net.axel.models.entities.Partner;
+import net.axel.models.entities.Ticket;
+import net.axel.repositories.TicketRepository;
+
+import java.sql.SQLException;
+import java.util.List;
+import java.util.UUID;
+
 public class TicketService {
+
+    private final TicketRepository ticketRepository;
+    private final ContractService contractService;
+
+    public TicketService() throws SQLException {
+        this.ticketRepository = new TicketRepository();
+        this.contractService = new ContractService();
+    }
+    public void addTicket(TicketDto dto) {
+        try {
+            final Contract contract = contractService.getContractById(dto.ContractId());
+            final Ticket ticket = new Ticket(UUID.randomUUID(), dto.transportType(), dto.purchasePrice(), dto.resellPrice(), dto.saleDate(), dto.ticketStatus(), contract);
+            ticketRepository.addTicket(ticket);
+        } catch (SQLException e) {
+            System.err.println("Error adding ticket: " + e.getMessage());
+        }
+    }
+    public List<Ticket> getAllTickets() {
+        try {
+            return ticketRepository.getAllTickets();
+        } catch (SQLException e) {
+            System.err.println("Error retrieving all tickets: " + e.getMessage());
+            return null;
+        }
+    }
+    public List<Ticket> getTicketsByPartner(UUID partnerId) {
+        try {
+            return ticketRepository.getTicketsByPartner(partnerId);
+        } catch (SQLException e) {
+            System.err.println("Error retrieving tickets for partner: " + e.getMessage());
+            return null;
+        }
+    }
+//    public Ticket findTicketById(UUID id) {
+//        try {
+//            List<Ticket> allTickets = getAllTickets();
+//            if (allTickets != null) {
+//                for (Ticket ticket : allTickets) {
+//                    if (ticket.getId().equals(id)) {
+//                        return ticket;
+//                    }
+//                }
+//            }
+//        } catch (Exception e) {
+//            System.err.println("Error finding ticket by ID: " + e.getMessage());
+//        }
+//        return null;
+//    }
+//    public void updateTicket(Ticket ticket) {
+//        try {
+//            ticketRepository.updateTicket(ticket);
+//        } catch (SQLException e) {
+//            System.err.println("Error updating ticket: " + e.getMessage());
+//        }
+//    }
+//    public void deleteTicket(UUID id) {
+//        try {
+//            ticketRepository.deleteTicket(id);
+//        } catch (SQLException e) {
+//            System.err.println("Error deleting ticket: " + e.getMessage());
+//        }
+//    }
 }
