a41c783d2686ada0dec122412261e4930df17fa1
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2024-09-04 14:07:36 +0100

    create dto for ticket

--- src/main/java/net/axel/models/dto/TicketDto.java
+++ src/main/java/net/axel/models/dto/TicketDto.java
@@ -1,4 +1,18 @@
-package net.axel.models;
+package net.axel.models.dto;
 
-public class TicketDto {
+import net.axel.models.enums.TicketStatus;
+import net.axel.models.enums.TransportType;
+
+import java.util.Date;
+import java.util.UUID;
+
+public record TicketDto (
+        TransportType transportType,
+        Double purchasePrice,
+        Double resellPrice,
+        Date saleDate,
+        TicketStatus ticketStatus,
+        UUID ContractId
+) {
+
 }
