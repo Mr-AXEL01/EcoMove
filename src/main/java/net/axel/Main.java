5eb675a0dce318da6627b9a642980a3eb04ec2d2
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2024-09-05 10:24:52 +0100

    instance the menu class and display the main menu

--- src/main/java/net/axel/Main.java
+++ src/main/java/net/axel/Main.java
@@ -1,7 +1,40 @@
 package net.axel;
 
+import net.axel.presentations.Menu;
+
+import java.sql.SQLException;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws SQLException {
+
+        System.out.println("                                                                                                                                                   \n" +
+                "                                                                                                                                                   \n" +
+                "EEEEEEEEEEEEEEEEEEEEEE                                     MMMMMMMM               MMMMMMMM                                                         \n" +
+                "E::::: W O R A ::::::E                                     M:::::::M             M:::::::M                                                         \n" +
+                "E::::::::::::::::::::E                                     M::::::::M           M::::::::M                                                         \n" +
+                "EE::::::EEEEEEEEE::::E                                     M:::::::::M         M:::::::::M                                                         \n" +
+                "  E:::::E       EEEEEE    cccccccccccccccc   ooooooooooo   M::::::::::M       M::::::::::M   ooooooooooo vvvvvvv           vvvvvvv eeeeeeeeeeee    \n" +
+                "  E:::::E               cc:::::::::::::::c oo:::::::::::oo M:::::::::::M     M:::::::::::M oo:::::::::::oov:::::v         v:::::vee::::::::::::ee  \n" +
+                "  E::::::EEEEEEEEEE    c:::::::::::::::::co:::::::::::::::oM:::::::M::::M   M::::M:::::::Mo:::::::::::::::ov:::::v       v:::::ve::::::eeeee:::::ee\n" +
+                "  E:::::::::::::::E   c:::::::cccccc:::::co:::::ooooo:::::oM::::::M M::::M M::::M M::::::Mo:::::ooooo:::::o v:::::v     v:::::ve::::::e     e:::::e\n" +
+                "  E:::::::::::::::E   c::::::c     ccccccco::::o     o::::oM::::::M  M::::M::::M  M::::::Mo::::o     o::::o  v:::::v   v:::::v e:::::::eeeee::::::e\n" +
+                "  E::::::EEEEEEEEEE   c:::::c             o::::o     o::::oM::::::M   M:::::::M   M::::::Mo::::o     o::::o   v:::::v v:::::v  e:::::::::::::::::e \n" +
+                "  E:::::E             c:::::c             o::::o     o::::oM::::::M    M:::::M    M::::::Mo::::o     o::::o    v:::::v:::::v   e::::::eeeeeeeeeee  \n" +
+                "  E:::::E       EEEEEEc::::::c     ccccccco::::o     o::::oM::::::M     MMMMM     M::::::Mo::::o     o::::o     v:::::::::v    e:::::::e           \n" +
+                "EE::::::EEEEEEEE:::::Ec:::::::cccccc:::::co:::::ooooo:::::oM::::::M               M::::::Mo:::::ooooo:::::o      v:::::::v     e::::::::e          \n" +
+                "E::::::::::::::::::::E c:::::::::::::::::co:::::::::::::::oM::::::M               M::::::Mo:::::::::::::::o       v:::::v       e::::::::eeeeeeee  \n" +
+                "E::::::::::::::::::::E  cc:::::::::::::::c oo:::::::::::oo M::::::M               M::::::M oo:::::::::::oo         v:::v         ee:::::::::::::e  \n" +
+                "EEEEEEEEEEEEEEEEEEEEEE    cccccccccccccccc   ooooooooooo   MMMMMMMM               MMMMMMMM   ooooooooooo            vvv            eeeeeeeeeeeeee  \n" +
+                "                                                                                                                                                   \n" +
+                "                                                                                                                                                   \n" +
+                "                                                                                                                                                   ");
+
+
+        try {
+            Menu menu = new Menu();
+            menu.showMainMenu();
+        } catch (SQLException e) {
+            System.err.println("Error initializing the application: " + e.getMessage());
+        }
     }
 }
