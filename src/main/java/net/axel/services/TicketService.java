8c9897e376c2cbfe225cb14f2b3aa630a3b91aa0
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2024-09-05 10:21:51 +0100

    set a check for the contract status to be inProgress

--- src/main/java/net/axel/services/TicketService.java
+++ src/main/java/net/axel/services/TicketService.java
@@ -3,6 +3,7 @@ package net.axel.services;
 import net.axel.models.dto.TicketDto;
 import net.axel.models.entities.Contract;
 import net.axel.models.entities.Ticket;
+import net.axel.models.enums.ContractStatus;
 import net.axel.repositories.TicketRepository;
 
 import java.sql.SQLException;
@@ -21,12 +22,17 @@ public class TicketService {
     public void addTicket(TicketDto dto) {
         try {
             final Contract contract = contractService.getContractById(dto.ContractId());
+            if(!contract.getContractStatus().equals(ContractStatus.IN_PROGRESS)) {
+                System.out.println("Can't create ticket , check contract status.");
+            } else {
             final Ticket ticket = new Ticket(UUID.randomUUID(), dto.transportType(), dto.purchasePrice(), dto.resellPrice(), dto.saleDate(), dto.ticketStatus(), contract);
             ticketRepository.addTicket(ticket);
+            }
         } catch (SQLException e) {
             System.err.println("Error adding ticket: " + e.getMessage());
         }
     }
+
     public List<Ticket> getAllTickets() {
         try {
             return ticketRepository.getAllTickets();
@@ -35,6 +41,7 @@ public class TicketService {
             return null;
         }
     }
+
     public List<Ticket> getTicketsByPartner(UUID partnerId) {
         try {
             return ticketRepository.getTicketsByPartner(partnerId);
@@ -43,6 +50,8 @@ public class TicketService {
             return null;
         }
     }
+
+
 //    public Ticket findTicketById(UUID id) {
 //        try {
 //            List<Ticket> allTickets = getAllTickets();
