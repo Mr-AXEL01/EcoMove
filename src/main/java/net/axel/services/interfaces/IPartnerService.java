08e3eb32182d1552f9f0feacfcc7da3900c57821
Mr-AXEL01 <abdelhak20aur02@gmail.com>
2024-09-08 23:48:35 +0100

    added : add methods signatures

--- src/main/java/net/axel/services/interfaces/IPartnerService.java
+++ src/main/java/net/axel/services/interfaces/IPartnerService.java
@@ -1,4 +1,14 @@
 package net.axel.services.interfaces;
 
+import net.axel.models.entities.Partner;
+
+import java.util.List;
+import java.util.UUID;
+
 public interface IPartnerService {
+    public void addPartner(Partner partner);
+    public Partner getPartnerById(UUID id);
+    public List<Partner> getAllPartners();
+    public void updatePartner(Partner partner);
+    public void deletePartner(UUID id);
 }
