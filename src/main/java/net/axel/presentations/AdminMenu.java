e5b1878c2548befe97bea330255e2720b168b480
Mr-AXEL01 <abdelhak20aur02@gmail.com>
2024-09-11 01:08:52 +0100

    refactored : update the menu to become admin menu.

--- src/main/java/net/axel/presentations/AdminMenu.java
+++ src/main/java/net/axel/presentations/AdminMenu.java
@@ -6,18 +6,20 @@ import net.axel.services.implementations.*;
 import java.sql.SQLException;
 import java.util.Scanner;
 
-public class Menu {
+public class AdminMenu {
 
     private final PartnerUi partnerUi;
     private final ContractUi contractUi;
     private final PromotionUi promotionUi;
     private final TicketUi ticketUi;
+    private final StationUi stationUi;
     private final Scanner scanner;
 
-    public Menu() throws SQLException {
+    public AdminMenu() throws SQLException {
         this.partnerUi = new PartnerUi(new PartnerService(new PartnerRepository()));
         this.contractUi = new ContractUi();
-        this.promotionUi = new PromotionUi(new PromotionService(new PromotionRepository() ,new ContractService(new ContractRepository(), new PartnerService(new PartnerRepository()))));
+        this.promotionUi = new PromotionUi(new PromotionService(new PromotionRepository(), new ContractService(new ContractRepository(), new PartnerService(new PartnerRepository()))));
+        this.stationUi = new StationUi(new StationService(new StationRepository()));
         this.ticketUi = new TicketUi(new TicketService(new TicketRepository(), new ContractService(new ContractRepository(), new PartnerService(new PartnerRepository())), new StationService(new StationRepository())), new PartnerService(new PartnerRepository()));
         this.scanner = new Scanner(System.in);
     }
@@ -25,35 +27,26 @@ public class Menu {
     public void showMainMenu() {
         int choice;
         do {
-            System.out.println("\n--- Main Management Menu ---\n");
+            System.out.println("\n--- Admin Management Menu ---\n");
             System.out.println("1. Partner Management");
             System.out.println("2. Contract Management");
             System.out.println("3. Promotion Management");
             System.out.println("4. Ticket Management");
-            System.out.println("5. Exit");
+            System.out.println("5. Station Management");
+            System.out.println("6. Exit");
             System.out.print("Enter your choice: ");
             choice = scanner.nextInt();
             scanner.nextLine();
 
             switch (choice) {
-                case 1:
-                    partnerUi.showMenu();
-                    break;
-                case 2:
-                    contractUi.showMenu();
-                    break;
-                case 3:
-                    promotionUi.showMenu();
-                    break;
-                case 4:
-                    ticketUi.showMenu();
-                    break;
-                case 5:
-                    System.out.println("Exiting the system...");
-                    break;
-                default:
-                    System.out.println("Invalid choice. Please try again.");
+                case 1 -> partnerUi.showMenu();
+                case 2 -> contractUi.showMenu();
+                case 3 -> promotionUi.showMenu();
+                case 4 -> ticketUi.showMenu();
+                case 5 -> stationUi.showMenu();
+                case 6 -> System.out.println("Exiting the system...");
+                default -> System.out.println("Invalid choice. Please try again.");
             }
-        } while (choice != 5);
+        } while (choice != 6);
     }
 }
